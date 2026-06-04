86281966f793fe545a9ab532b2f71dc80373b4a2
Legopitstop <1589lego@gmail.com>
2026-06-04 01:12:00 -0500

    style(server-graphics): format atmospherics setters

--- libraries/minecraft-server-graphics-mock/src/index.ts
+++ libraries/minecraft-server-graphics-mock/src/index.ts
@@ -11,46 +11,16 @@ export class Atmospherics {
   resetSkyZenithColor(biomeIdentifier: BiomeType) {}
   resetSunGlareShape(biomeIdentifier: BiomeType) {}
   resetSunMieStrength(biomeIdentifier: BiomeType) {}
-  setHorizonBlendMax(
-    blendMax: number | Record<number, number>,
-    biomeIdentifier: BiomeType,
-  ) {}
-  setHorizonBlendMieStart(
-    blendMieStart: number | Record<number, number>,
-    biomeIdentifier: BiomeType,
-  ) {}
-  setHorizonBlendMin(
-    blendMin: number | Record<number, number>,
-    biomeIdentifier: BiomeType,
-  ) {}
-  setHorizonBlendStart(
-    blendStart: number | Record<number, number>,
-    biomeIdentifier: BiomeType,
-  ) {}
-  setMoonMieStrength(
-    moonMieStrength: number | Record<number, number>,
-    biomeIdentifier: BiomeType,
-  ) {}
-  setRayleighStrength(
-    rayleighStrength: number | Record<number, number>,
-    biomeIdentifier: BiomeType,
-  ) {}
-  setSkyHorizonColor(
-    color: RGB | Record<number, RGB>,
-    biomeIdentifier: BiomeType,
-  ) {}
-  setSkyZenithColor(
-    color: RGB | Record<number, RGB>,
-    biomeIdentifier: BiomeType,
-  ) {}
-  setSunGlareShape(
-    sunFlareShape: number | Record<number, number>,
-    biomeIdentifier: BiomeType,
-  ) {}
-  setSunMieStrength(
-    sunMieStrength: number | Record<number, number>,
-    biomeIdentifier: BiomeType,
-  ) {}
+  setHorizonBlendMax(blendMax: number | Record<number, number>, biomeIdentifier: BiomeType) {}
+  setHorizonBlendMieStart(blendMieStart: number | Record<number, number>, biomeIdentifier: BiomeType) {}
+  setHorizonBlendMin(blendMin: number | Record<number, number>, biomeIdentifier: BiomeType) {}
+  setHorizonBlendStart(blendStart: number | Record<number, number>, biomeIdentifier: BiomeType) {}
+  setMoonMieStrength(moonMieStrength: number | Record<number, number>, biomeIdentifier: BiomeType) {}
+  setRayleighStrength(rayleighStrength: number | Record<number, number>, biomeIdentifier: BiomeType) {}
+  setSkyHorizonColor(color: RGB | Record<number, RGB>, biomeIdentifier: BiomeType) {}
+  setSkyZenithColor(color: RGB | Record<number, RGB>, biomeIdentifier: BiomeType) {}
+  setSunGlareShape(sunFlareShape: number | Record<number, number>, biomeIdentifier: BiomeType) {}
+  setSunMieStrength(sunMieStrength: number | Record<number, number>, biomeIdentifier: BiomeType) {}
 }
 
 export const atmosphere = new Atmospherics();
